358f8fd6f2025f4dca3f03dc353d2347896c530f
Egan <dunninea@plu.edu>
2017-06-26 09:30:20 -0700

    more content

---
+++ header-learncrypto.js
@@ -0,0 +1,6 @@
+document.write(
+"<table><tr>" +
+"<td><a href='index.html'>Home</a></td>" +
+"<td><a href='learncrypto-index.html'>Learn Crypto Index</a></td>" +
+"</tr></table>"
+);
---
+++ header.js
@@ -0,0 +1,6 @@
+document.write(
+"<table><tr>" +
+"<td><a href='index.html'>Home</a></td>" +
+"<td><a href='projects.html'>Projects</a></td>" +
+"</tr></table>"
+);
