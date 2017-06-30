d2c1e9c0d82d020da080a1ca655317b5bcfd8b1d
Egan <dunninea@plu.edu>
2017-06-30 16:17:23 -0700

    changed format and added content

--- header.js
+++ header.js
@@ -1,6 +1,8 @@
 document.write(
+"<strong>Egan Dunning</Strong>" +
 "<table><tr>" +
-"<td><a href='index.html'>Home</a></td>" +
+"<td><a href='index.html'>Home</a> </td>" +
 "<td><a href='projects.html'>Projects</a></td>" +
+"<td><a href='documents/resume6-4.pdf'>Resume</a></td>" +
 "</tr></table>"
 );
