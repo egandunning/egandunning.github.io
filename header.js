ec994ce6b963d5766cef1814d7243f16ca4f38e4
egandunning <dunninea@plu.edu>
2017-11-24 20:45:37 -0500

    put navigation links in nav tag

--- header.js
+++ header.js
@@ -1,8 +1,8 @@
 document.write(
 "<strong style='font-size:150%'>Egan Dunning</strong>" +
-"<table><tr>" +
+"<nav><table><tr>" +
 "<td><a href='index.html'>Home</a> </td>" +
 "<td><a href='projects.html'>Projects</a></td>" +
 "<td><a href='documents/resume11-19.pdf'>Resume</a></td>" +
-"</tr></table>"
+"</tr></table></nav>"
 );
