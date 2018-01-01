0b94d9c456381865b2973848d9112094be628529
egandunning <dunninea@plu.edu>
2018-01-01 12:20:03 -0500

    changed year to 2018

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write(
-'Egan Dunning 2017<br>' +
+'Egan Dunning 2018<br>' +
 '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">' +
 '<img alt="Creative Commons License" style="border-width:0" ' +
 'src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>'+
