bbf6671c8f2d0818d1f144bd1bdcded9ca3a4fbd
egandunning <dunninea@plu.edu>
2018-03-12 08:21:29 -0500

    updated resume

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ document.write(
 "<nav><table><tr>" +
 "<td><a href='index.html'>Home</a> </td>" +
 "<td><a href='projects.html'>Projects</a></td>" +
-"<td><a href='documents/resume1-29-18.pdf' target='_blank'>Resume</a></td>" +
+"<td><a href='documents/resume3-12-18.pdf' target='_blank'>Resume</a></td>" +
 "<td><a href='piday2018.html'>Pi Day 2018</a></td>" +
 "</tr></table></nav>"
 );
