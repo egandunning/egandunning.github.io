e6edd00be8036c7d11c312eb6c49c2a1c3140724
Egan <dunninea@plu.edu>
2018-04-05 20:44:41 -0500

    restructured website

---
+++ projects/header.js
@@ -0,0 +1,10 @@
+//I'm sorry
+document.write(
+"<strong style='font-size:150%'>Egan Dunning</strong>" +
+"<nav><table><tr>" +
+"<td><a href='../index.html'>Home</a> </td>" +
+"<td><a href='../projects.html'>Projects</a></td>" +
+"<td><a href='../documents/resume3-12-18.pdf' target='_blank'>Resume</a></td>" +
+"<td><a href='../piday2018.html'>Pi Day 2018</a></td>" +
+"</tr></table></nav>"
+);
