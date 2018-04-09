272abeb7dd863d0bccd447bbb65c4a41fe9bc3c1
Egan <dunninea@plu.edu>
2018-04-08 21:47:21 -0500

    linked to blog index in header

--- header.js
+++ header.js
@@ -5,6 +5,7 @@ document.write(
 "<td><a href='index.html'>Home</a> </td>" +
 "<td><a href='projects.html'>Projects</a></td>" +
 "<td><a href='documents/resume3-12-18.pdf' target='_blank'>Resume</a></td>" +
+"<td><a href='blog.html'>Blog</a></td>" +
 "<td><a href='piday2018.html'>Pi Day 2018</a></td>" +
 "</tr></table></nav>"
 );
