c8047209f8ac83d7284526580e09cdc35b49a3a1
Egan <dunninea@plu.edu>
2018-04-26 18:31:45 -0500

    finished code to html functionality

--- prettycode.py
+++ prettycode.py
@@ -5,6 +5,7 @@ import sys
 import os
 
 tab = "  " #change if you want bigger/smaller tabs
+codeTab = "    " #change if code indent width != 4 spaces
 
 def prettify():
     if(len(sys.argv) != 2):
@@ -15,13 +16,18 @@ def prettify():
         print("invalid file")
         return 1
 
-    codeFile = os.open(sys.argv[1], "r")
+    codeFile = open(sys.argv[1], "r")
 
     #read no more than 10KB
     code = codeFile.read(10000)
     codeFile.close()
 
+    codeLines = code.splitlines()
+
     print('<div class="code">')
     print(tab + '<code>')
-    #todo: print code, prettified
+    for line in codeLines:
+        print(tab + tab + line.replace('    ', '&nbsp&nbsp '))
     print(tab + '</code>')
+
+prettify()
