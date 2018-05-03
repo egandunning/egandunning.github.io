3e70b9edfd48351e95fa19d51f111371156eca00
Egan <dunninea@plu.edu>
2018-05-02 19:05:10 -0500

    linked linkedin profile

--- header.js
+++ header.js
@@ -6,6 +6,7 @@ document.write(
 "<td><a href='projects.html'>Projects</a></td>" +
 "<td><a href='documents/resume3-12-18.pdf' target='_blank'>Resume</a></td>" +
 "<td><a href='blog.html'>Blog</a></td>" +
+"<td><a href='https://www.linkedin.com/in/egan-dunning-262648a1' target='_blank'>Linkedin</a></td>" +
 "<td><a href='piday2018.html'>Pi Day 2018</a></td>" +
 "</tr></table></nav>"
 );
