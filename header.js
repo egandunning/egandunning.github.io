acd56c0690d578184a2e4efce9abbf6465c7be85
Egan <dunninea@plu.edu>
2018-05-03 18:51:24 -0500

    updated header

--- header.js
+++ header.js
@@ -1,12 +1,10 @@
 //I'm sorry
 document.write(
-"<strong style='font-size:150%'>Egan Dunning</strong>" +
-"<nav><table><tr>" +
-"<td><a href='index.html'>Home</a> </td>" +
-"<td><a href='projects.html'>Projects</a></td>" +
-"<td><a href='documents/resume3-12-18.pdf' target='_blank'>Resume</a></td>" +
-"<td><a href='blog.html'>Blog</a></td>" +
-"<td><a href='https://www.linkedin.com/in/egan-dunning-262648a1' target='_blank'>Linkedin</a></td>" +
-"<td><a href='piday2018.html'>Pi Day 2018</a></td>" +
-"</tr></table></nav>"
+"<ul>"+
+"<li><a href='index.html'><strong>Egan Dunning</strong></a></li>"+
+"<li><a href='projects.html'>Projects</a></li>"+
+"<li><a href='documents/resume3-12-18.pdf' target='_blank'>Resume</a></li>"+
+"<li><a href='blog.html'>Blog</a></li>"+
+"<li><a href='https://www.linkedin.com/in/egan-dunning-262648a1' target='_blank'>Linkedin</a></li>"+
+"</ul>"
 );
