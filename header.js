9a8b02551bca6bdc8a5c246ae9a93f81d48ee0b3
Egan Dunning <dunninea@plu.edu>
2018-09-18 21:46:10 -0700

    updated resume

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ document.write(
 "<ul>"+
 "<li><a href='index.html'><strong>Egan Dunning</strong></a></li>"+
 "<li><a href='projects.html'>Projects</a></li>"+
-"<li><a href='documents/resume3-12-18.pdf' target='_blank'>Resume</a></li>"+
+"<li><a href='documents/resume9-18-18.pdf' target='_blank'>Resume</a></li>"+
 "<li><a href='blog.html'>Blog</a></li>"+
 "<li><a href='https://www.linkedin.com/in/egan-dunning-262648a1' target='_blank'>Linkedin</a></li>"+
 "</ul>"
