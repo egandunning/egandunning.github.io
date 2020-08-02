8c837079866bde5a35e4decd55ea709c7f2f3cf8
Egan <dunninea@plu.edu>
2020-08-01 18:01:17 -0700

    header uses current resume

--- header.js
+++ header.js
@@ -1,9 +1,8 @@
-//I'm sorry
 document.write(
 "<ul>"+
 "<li><a href='index.html'><strong>Egan Dunning</strong></a></li>"+
 "<li><a href='projects.html'>Projects</a></li>"+
-"<li><a href='documents/resume04-07-19.pdf' target='_blank'>Resume</a></li>"+
+"<li><a href='documents/resume_current.pdf' target='_blank'>Resume</a></li>"+
 "<li><a href='blog.html'>Blog</a></li>"+
 "<li><a href='https://www.linkedin.com/in/egan-dunning-262648a1' target='_blank'>Linkedin</a></li>"+
 "</ul>"
