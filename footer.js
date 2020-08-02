4cf7e74d98201081b10f9fcbf6a092c97771d77a
Egan <dunninea@plu.edu>
2020-08-01 18:06:40 -0700

    bump year

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write(
-'Egan Dunning 2019<br>' +
+'Egan Dunning 2020<br>' +
 '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">' +
 '<img alt="Creative Commons License" style="border-width:0" ' +
 'src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>'+
