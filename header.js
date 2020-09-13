f6e0d15c673c54ada81689c95889adffb3400dc9
Egan <dunninea@plu.edu>
2020-09-12 22:23:08 -0700

    more recipe stuff

--- header.js
+++ header.js
@@ -3,6 +3,7 @@ document.write(
 "<li><a href='index.html'><strong>Egan Dunning</strong></a></li>"+
 "<li><a href='projects.html'>Projects</a></li>"+
 "<li><a href='documents/resume_current.pdf' target='_blank'>Resume</a></li>"+
+"<li><a href='recipes.html'>Recipes</a></li>"+
 "<li><a href='blog.html'>Blog</a></li>"+
 "<li><a href='https://www.linkedin.com/in/egan-dunning-262648a1' target='_blank'>Linkedin</a></li>"+
 "</ul>"
